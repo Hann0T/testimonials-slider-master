ac988b25f297843a703a361a5bd8b499b1c40ca9
Hans <hansolivas1@gmail.com>
2021-01-25 16:45:02 -0500

    final commit

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,68 @@
-D:\Development\WebDev\coding-bootcamp-testimonials-slider-master\Js
+const JOHN = document.querySelectorAll(".john")
+const TANYA = document.querySelectorAll('.tanya')
+
+const SCENES = [TANYA, JOHN]
+
+let scene = 0
+let currentScene = SCENES[scene]
+
+function prevSlider() {
+	if (scene === 0) {
+		scene = SCENES.length - 1
+		currentScene = SCENES[scene]
+	} else {
+		scene--
+		currentScene = SCENES[scene]
+	}
+
+	drawNewScene(currentScene)
+}
+
+function nextSlider() {
+	if (scene === SCENES.length - 1) {
+		scene = 0
+		currentScene = SCENES[scene]
+	} else {
+		scene++
+		currentScene = SCENES[scene]
+	}
+	drawNewScene(currentScene)
+}
+
+console.log(currentScene)
+console.log(SCENES)
+
+
+function drawNewScene2(currentScene) {
+
+	SCENES.forEach(scene => {
+		if (scene.classList[2]) {
+			console.log('nadaparahacer')
+		} else {
+			scene.classList.remove('isActive')
+			scene.classList.add('isNoActive')
+		}
+	})
+
+	currentScene.classList.remove('isNoActive')
+	currentScene.classList.add('isActive')
+}
+
+function drawNewScene(currentScene) {
+	SCENES.forEach(scene => {
+		scene.forEach(thing => {
+			if (thing.classList[2] === 'isNoActive') {
+				thing.classList.remove('isNoActive')
+				thing.classList.add('isActive')
+			} else {
+				thing.classList.remove('isActive')
+				thing.classList.add('isNoActive')
+			}
+		})
+	})
+}
+
+
+
+
+
